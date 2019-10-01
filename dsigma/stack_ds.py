fe8407ec04bea0772103a270f9285d347fde8518
dr-guangtou <dr.guangtou@gmail.com>
2019-10-01 12:50:23 -0700

    Add cov_with_mask and batch_dsig_cov functions

--- dsigma/stack_ds.py
+++ dsigma/stack_ds.py
@@ -6,10 +6,13 @@ from functools import partial
 import numpy as np
 
 from . import jackknife as jk
+from . import functions as fu
 from . import compute_ds as ds
+from . import covariance as cov
 from . import data_structure as data
 
-__all__ = ["form_delta_sigma", "stack_delta_sigma", "batch_delta_sigma"]
+__all__ = ["form_delta_sigma", "stack_delta_sigma", "stack_with_mask",
+           "batch_delta_sigma", "batch_dsig_cov", "cov_with_mask"]
 
 
 def form_delta_sigma(lens_ds, rand_ds, lens_data, rand_data, radial_bins,
@@ -187,21 +190,42 @@ def stack_with_mask(lens_ds, lens_data, rand_ds, rand_data, radial_bins, lens_ma
     return dsigma_out
 
 
-def batch_delta_sigma(lens_pre,
-                      lens_data,
-                      mask_list,
-                      rand_pre=None,
-                      rand_data=None,
-                      pickle_output=None,
-                      njackknife_fields=31,
-                      boost_factor=False,
-                      selection_bias=False,
-                      weight_field='weight',
-                      rand_zweight_nbins=10,
-                      same_weight_rand=True,
-                      save=False,
-                      qa=False,
-                      output_prefix='delta_sigma'):
+def cov_with_mask(lens_ds, lens_data, rand_ds, rand_data, lens_mask,
+                  boost_factor=False, selection_bias=False, weight_field='weight',
+                  rand_zweight_nbins=10, n_jobs=4, n_boots=1000,
+                  boxsize=1, trunc=0.2, same_weight_rand=True):
+    """Simple wrapper to generate DSigma profile with bootstrap resampling and
+    covariance.
+    """
+    # Select a subsample of lenses if mask is available
+    if lens_mask is not None:
+        assert len(lens_mask) == len(lens_ds), "\n# Wrong mask size!"
+        print("#    Sample: %d / %d galaxies" % (sum(lens_mask), len(lens_ds)))
+        lens_ds_use, lens_data_use = lens_ds[lens_mask], lens_data[lens_mask]
+    else:
+        lens_ds_use, lens_data_use = lens_ds, lens_data
+
+    # Gather the Bootstrap samples
+    dsig_all, dsig_boot, dsig_jk = cov.get_bootstrap_samples_dsigma(
+        lens_ds_use, rand_ds, lens_data_use, rand_data,
+        n_boots=n_boots, n_jobs=n_jobs,
+        z_bins=rand_zweight_nbins, selection_bias=selection_bias,
+        weight_field=weight_field, use_boost=boost_factor,
+        same_weight_rand=same_weight_rand)
+
+    # Get the correlation and covariance matrix
+    cor_org, cor_trunc, cov_trunc = fu.smooth_cov(
+        dsig_boot, boxsize=boxsize, trunc=trunc)
+
+    return {'dsig_all': dsig_all, 'dsig_boot': dsig_boot, 'dsig_jk': dsig_jk,
+            'cor_org': cor_org, 'cor_trunc': cor_trunc, 'cov_trunc': cov_trunc}
+
+
+def batch_delta_sigma(lens_pre, lens_data, mask_list,
+                      rand_pre=None, rand_data=None, pickle_output=None,
+                      njackknife_fields=31, boost_factor=False, selection_bias=False,
+                      weight_field='weight', rand_zweight_nbins=10, same_weight_rand=True,
+                      save=False, qa=False, output_prefix='dsigma'):
     """Stacked Delta Sigma signals using pre-compute result.
 
     Parameters
@@ -266,6 +290,83 @@ def batch_delta_sigma(lens_pre,
                            same_weight_rand=same_weight_rand,
                            save=save, qa=qa,
                            output_prefix=output_prefix)
+
+    # We need to know the order of the mask, so do not use multiprocessing
+    results = [single_stack(lens_mask=mask) for mask in mask_list]
+
+    if pickle_output is not None:
+        pickle.dump(results, open(pickle_output, 'wb'))
+
+    return results
+
+
+def batch_dsig_cov(lens_pre, lens_data, mask_list,
+                   rand_pre=None, rand_data=None, pickle_output=None,
+                   njackknife_fields=31, boost_factor=False, selection_bias=False,
+                   weight_field='weight', rand_zweight_nbins=10, same_weight_rand=True,
+                   n_jobs=4, n_boots=1000, boxsize=1, trunc=0.2):
+    """Stacked Delta Sigma signals using pre-compute result.
+
+    Parameters
+    ----------
+    lens_pre: numpy array
+        Precomputed lensing results for leses.
+    lens_data : numpy array
+        Lens catalog.
+    rand_pre: numpy array
+        Precomputed lensing results for random objects.
+    rand_data : numpy array
+        Random catalog.
+    mask_list: list of boolen array
+        List of lens masks.
+    rand_ds: numpy array, optional
+        Precomputed lensing results for random.  Default: None
+    lens_mask: boolen array, optional
+        Boolen array for selecting lens to stacking.
+    njackknife_fields: int, optional
+        Number of sub-regions for Jackknife resampling.  Default: 41
+    pickle_output: str, optional
+        Name of the output pickle file.  Default: None
+    same_weight_rand : bool, optional
+        Use the same weight column for random or not.  Default: True
+
+    Return
+    ------
+    results : list of structured numpy array
+        Stacked DeltaSigma results for each selection of lens.
+    """
+    # Make sure the pre-compute results and the lens catalog are consistent
+    # TODO: this step takes long time
+    ds.assert_precompute_catalog_consistent(lens_pre, lens_data)
+
+    # Get the pre-compute deltaSigma data, radial bins
+    lens_ds = lens_pre['delta_sigma']
+    rand_ds = rand_pre["delta_sigma"] if rand_pre is not None else None
+
+    # If available, make sure the random and lens have consistent pre-compute settings.
+    if rand_pre is not None:
+        ds.assert_lens_rand_consistent(lens_pre, rand_pre)
+        if rand_data is None:
+            raise Exception("# Need the random object catalog!")
+        else:
+            assert len(rand_data) == len(rand_ds)
+
+    # Assign the jackknife regions if necessary
+    if rand_ds is not None:
+        lens_ds, rand_ds = jk.add_jackknife_both(lens_ds, rand_ds, njackknife_fields)
+    else:
+        lens_ds = jk.add_jackknife_field(lens_ds, njackknife_fields)
+
+    single_stack = partial(cov_with_mask,
+                           lens_ds=lens_ds, lens_data=lens_data,
+                           rand_ds=rand_ds, rand_data=rand_data,
+                           boost_factor=boost_factor,
+                           selection_bias=selection_bias,
+                           weight_field=weight_field,
+                           rand_zweight_nbins=rand_zweight_nbins,
+                           same_weight_rand=same_weight_rand,
+                           n_jobs=n_jobs, n_boots=n_boots,
+                           boxsize=boxsize, trunc=trunc)
 
     # We need to know the order of the mask, so do not use multiprocessing
     results = [single_stack(lens_mask=mask) for mask in mask_list]
