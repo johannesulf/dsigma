55f376c236188371a6a4fddf6eb5aea750148571
Johannes Lange <jolange@ucsc.edu>
2021-05-17 09:58:14 -0700

    updated API, separated tangential shear and excess surface density in stacking

--- dsigma/stacking.py
+++ dsigma/stacking.py
@@ -7,13 +7,13 @@ from .physics import mpc_per_degree, lens_magnification_shear_bias
 __all__ = ['number_of_pairs', 'raw_tangential_shear',
            'raw_excess_surface_density', 'photo_z_dilution_factor',
            'boost_factor', 'scalar_shear_response_factor',
-           'tensor_shear_response_factor', 'shear_responsivity_factor',
+           'matrix_shear_response_factor', 'shear_responsivity_factor',
            'mean_lens_redshift', 'mean_source_redshift',
-           'mean_critical_surface_density', 'excess_surface_density',
-           'shape_noise_error']
+           'mean_critical_surface_density', 'tangential_shear',
+           'excess_surface_density', 'shape_noise_error']
 
 
-def number_of_pairs(table_l, rotation=False):
+def number_of_pairs(table_l):
     """Compute the number of lens-source pairs per bin.
 
     Parameters
@@ -30,7 +30,7 @@ def number_of_pairs(table_l, rotation=False):
     return np.sum(table_l['sum 1'].data, axis=0)
 
 
-def raw_tangential_shear(table_l, rotation=False):
+def raw_tangential_shear(table_l):
     """Compute the average tangential shear for a catalog.
 
     Parameters
@@ -138,7 +138,7 @@ def scalar_shear_response_factor(table_l):
                table_l['w_sys'].data[:, None], axis=0))
 
 
-def tensor_shear_response_factor(table_l):
+def matrix_shear_response_factor(table_l):
     r"""Compute the mean tangential response :math:`R_t` factor such that
     :math:`\gamma_{\mathrm obs} = R_t \gamma_{\mathrm intrinsic}`.
 
@@ -278,17 +278,15 @@ def lens_magnification_bias(table_l, alpha_l, camb_results,
         range(len(theta))]) * sigma_crit
 
 
-def excess_surface_density(table_l, table_r=None,
-                           photo_z_dilution_correction=False,
-                           boost_correction=False,
-                           scalar_shear_response_correction=False,
-                           tensor_shear_response_correction=False,
-                           shear_responsivity_correction=False,
-                           hsc_selection_bias_correction=False,
-                           random_subtraction=False,
-                           return_table=False):
-    """Compute the total lensing signal, including all corrections from
-    precompute results.
+def tangential_shear(table_l, table_r=None, photo_z_dilution_correction=False,
+                     boost_correction=False,
+                     scalar_shear_response_correction=False,
+                     matrix_shear_response_correction=False,
+                     shear_responsivity_correction=False,
+                     hsc_selection_bias_correction=False,
+                     random_subtraction=False, return_table=False):
+    """Compute the total excess surface density signal (including all
+    corrections) from precompute results.
 
     Parameters
     ----------
@@ -296,15 +294,12 @@ def excess_surface_density(table_l, table_r=None,
         Precompute results for the lenses.
     table_r : astropy.table.Table, optional
         Precompute results for random lenses.
-    photo_z_dilution_correction : boolean, optional
-        If true, correct for photo-z biases. This can only be done if a
-        calibration catalog has been provided in the Precomputation phase.
     boost_correction : boolean, optional
         If true, calculate and apply a boost factor correction. This can only
         be done if a random catalog is provided.
     scalar_shear_response_correction : boolean or string, optional
         Whether to correct for the multiplicative shear bias (scalar form).
-    tensor_shear_response_correction : boolean or string, optional
+    matrix_shear_response_correction : boolean or string, optional
         Whether to correct for the multiplicative shear bias (tensor form).
     shear_responsivity_correction : boolean, optional
         If true, correct for the shear responsivity.
@@ -329,9 +324,8 @@ def excess_surface_density(table_l, table_r=None,
 
     result['rp_min'] = table_l.meta['bins'][:-1]
     result['rp_max'] = table_l.meta['bins'][1:]
+    result['n_pairs'] = number_of_pairs(table_l)
     result['rp'] = np.sqrt(result['rp_min'] * result['rp_max'])
-    result['ds_raw'] = raw_excess_surface_density(table_l)
-    result['ds'] = raw_excess_surface_density(table_l)
     result['et_raw'] = raw_tangential_shear(table_l)
     result['et'] = raw_tangential_shear(table_l)
     result['z_l'] = mean_lens_redshift(table_l)
@@ -342,29 +336,125 @@ def excess_surface_density(table_l, table_r=None,
             raise Exception('Cannot compute boost factor correction without ' +
                             'results from a random catalog.')
         result['b'] = boost_factor(table_l, table_r)
-        result['ds'] *= result['b']
         result['et'] *= result['b']
 
     if scalar_shear_response_correction:
         result['1+m'] = 1 + scalar_shear_response_factor(table_l)
-        result['ds'] /= result['1+m']
         result['et'] /= result['1+m']
 
-    if tensor_shear_response_correction:
-        result['R_t'] = tensor_shear_response_factor(table_l)
-        result['ds'] /= result['R_t']
+    if matrix_shear_response_correction:
+        result['R_t'] = matrix_shear_response_factor(table_l)
         result['et'] /= result['R_t']
 
     if shear_responsivity_correction:
         result['2R'] = 2 * shear_responsivity_factor(table_l)
-        result['ds'] /= result['2R']
         result['et'] /= result['2R']
 
     if hsc_selection_bias_correction:
         result['1+m_sel'] = 1 + surveys.hsc.selection_bias_factor(
             table_l)
-        result['ds'] *= result['1+m_sel']
         result['et'] *= result['1+m_sel']
+
+    if random_subtraction:
+        if table_r is None:
+            raise Exception('Cannot subtract random results without ' +
+                            'results from a random catalog.')
+        result['et_r'] = excess_surface_density(
+            table_r, boost_correction=False,
+            scalar_shear_response_correction=scalar_shear_response_correction,
+            matrix_shear_response_correction=matrix_shear_response_correction,
+            shear_responsivity_correction=shear_responsivity_correction,
+            hsc_selection_bias_correction=hsc_selection_bias_correction,
+            random_subtraction=False, return_table=False)
+        result['et'] -= result['et_r']
+
+    if not return_table:
+        return result['et']
+
+    return result
+
+
+def excess_surface_density(table_l, table_r=None,
+                           photo_z_dilution_correction=False,
+                           boost_correction=False,
+                           scalar_shear_response_correction=False,
+                           matrix_shear_response_correction=False,
+                           shear_responsivity_correction=False,
+                           hsc_selection_bias_correction=False,
+                           random_subtraction=False,
+                           return_table=False):
+    """Compute the total excess surface density signal (including all
+    corrections) from precompute results.
+
+    Parameters
+    ----------
+    table_l : astropy.table.Table
+        Precompute results for the lenses.
+    table_r : astropy.table.Table, optional
+        Precompute results for random lenses.
+    photo_z_dilution_correction : boolean, optional
+        If true, correct for photo-z biases. This can only be done if a
+        calibration catalog has been provided in the Precomputation phase.
+    boost_correction : boolean, optional
+        If true, calculate and apply a boost factor correction. This can only
+        be done if a random catalog is provided.
+    scalar_shear_response_correction : boolean or string, optional
+        Whether to correct for the multiplicative shear bias (scalar form).
+    matrix_shear_response_correction : boolean or string, optional
+        Whether to correct for the multiplicative shear bias (tensor form).
+    shear_responsivity_correction : boolean, optional
+        If true, correct for the shear responsivity.
+    hsc_selection_bias_correction : boolean, optional
+        If true, correct for the multiplicative selection bias in HSC.
+    random_subtraction : boolean, optional
+        If true, subtract the signal around randoms. This can only be done if
+        a random catalog is provided.
+    return_table : boolean, optional
+        If true, return a table with many intermediate steps of the
+        computation. Otherwise, a simple array with just the final excess
+        surface density is returned.
+
+    Returns
+    -------
+    delta_sigma : numpy array or astropy.table.Table
+        The excess surface density in each radial bin specified in the
+        precomputation phase.
+    """
+
+    result = Table()
+
+    result['rp_min'] = table_l.meta['bins'][:-1]
+    result['rp_max'] = table_l.meta['bins'][1:]
+    result['n_pairs'] = number_of_pairs(table_l)
+    result['rp'] = np.sqrt(result['rp_min'] * result['rp_max'])
+    result['ds_raw'] = raw_excess_surface_density(table_l)
+    result['ds'] = raw_excess_surface_density(table_l)
+    result['z_l'] = mean_lens_redshift(table_l)
+    result['z_s'] = mean_source_redshift(table_l)
+
+    if boost_correction:
+        if table_r is None:
+            raise Exception('Cannot compute boost factor correction without ' +
+                            'results from a random catalog.')
+        result['b'] = boost_factor(table_l, table_r)
+        result['ds'] *= result['b']
+
+    if scalar_shear_response_correction:
+        result['1+m'] = 1 + scalar_shear_response_factor(table_l)
+        result['ds'] /= result['1+m']
+
+    if matrix_shear_response_correction:
+        result['R_t'] = matrix_shear_response_factor(table_l)
+        result['ds'] /= result['R_t']
+
+    if shear_responsivity_correction:
+        result['2R'] = 2 * shear_responsivity_factor(table_l)
+        result['ds'] /= result['2R']
+
+    if hsc_selection_bias_correction:
+        result['1+m_sel'] = 1 + surveys.hsc.selection_bias_factor(
+            table_l)
+        result['ds'] *= result['1+m_sel']
 
     if photo_z_dilution_correction:
         result['f_bias'] = photo_z_dilution_factor(table_l)
@@ -374,18 +464,15 @@ def excess_surface_density(table_l, table_r=None,
         if table_r is None:
             raise Exception('Cannot subtract random results without ' +
                             'results from a random catalog.')
-        result_r = excess_surface_density(
+        result['ds_r'] = excess_surface_density(
             table_r, photo_z_dilution_correction=photo_z_dilution_correction,
             boost_correction=False,
             scalar_shear_response_correction=scalar_shear_response_correction,
-            tensor_shear_response_correction=tensor_shear_response_correction,
+            matrix_shear_response_correction=matrix_shear_response_correction,
             shear_responsivity_correction=shear_responsivity_correction,
             hsc_selection_bias_correction=hsc_selection_bias_correction,
-            random_subtraction=False, return_table=True)
-        result['ds_r'] = result_r['ds']
-        result['et_r'] = result_r['et']
+            random_subtraction=False, return_table=False)
         result['ds'] -= result['ds_r']
-        result['et'] -= result['et_r']
 
     if not return_table:
         return result['ds']
@@ -394,9 +481,9 @@ def excess_surface_density(table_l, table_r=None,
 
 
 def shape_noise_error(table_l, table_r=None, **kwargs):
-    """Analytically estimate the shape noise error. Note that on large scales,
-    there is additional cosmic variance error. Thus, jackknife should be used
-    for those scales.
+    """Analytically estimate the shape noise error for the excess surface
+    density. Note that on large scales, there is additional cosmic variance
+    error. Thus, jackknife re-sampling should be used for those scales.
 
     Parameters
     ----------
