494a14cee8bcef5fdc326b8bbaf4a914acfeaa95
Johannes U. Lange <julange.astro@pm.me>
2022-11-06 19:37:43 -0500

    updated documentation, added logo (#1)

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'dsigma'
-copyright = '2021, Johannes Lange'
+copyright = '2022, Johannes Lange'
 author = 'Johannes Lange'
 
 # The short X.Y version
@@ -71,6 +71,12 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'dsigma.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+    'titles_only': True,
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
