ed42020ec7edfb98f7b56074a717581402d26bea
Johannes U. Lange <julange.astro@pm.me>
2024-05-19 18:41:53 -0400

    disable -march=native for Apple silicon

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup, Extension
+import platform
+
 from Cython.Build import cythonize
+from setuptools import setup, Extension
+
+extra_compile_args = ['-O3']
+
+if not (platform.system() == "Darwin" and platform.machine() == "arm64"):
+    extra_compile_args += ['-march=native']
 
 setup(
     ext_modules=cythonize([Extension(
         'dsigma.precompute_engine', ['dsigma/precompute_engine.pyx'],
-        extra_compile_args=['-O3', '-march=native'])])
+        extra_compile_args=extra_compile_args)])
 )
